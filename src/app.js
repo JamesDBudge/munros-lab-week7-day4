d0949ed568a98ef283041e809ed426e305871659
James Budge <thebudgie@gmail.com>
2019-05-16 15:47:39 +0100

    map looks sweet

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const SelectView = require('./views/select_view.js')
 const Munros = require('./models/munros.js')
 const MunroListView = require('./views/munro_list_view.js')
+const MapView = require('./views/map_view.js')
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  const benNevis = [56.79, -5.00]
+  const mapdiv = document.querySelector('#map')
+  const mapView = new MapView(mapdiv, benNevis, 7)
+  mapView.init()
+  mapView.bindEvents()
 
   const selectContainer = document.querySelector('#region-select')
   const selectView = new SelectView(selectContainer)
